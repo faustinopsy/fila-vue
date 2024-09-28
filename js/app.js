4d8332eb3c6f56c1dfee8d52dd8254d58b20590e
faustinopsy <rodrigohipnose@gmail.com>
2024-09-28 16:15:38 -0300

    remoção do lixo

--- js/app.js
+++ js/app.js
@@ -39,32 +39,6 @@ const app = Vue.createApp({
         </ul>
         </div>
         `,
-        style:
-        `
-        .container {
-        position: relative;
-        padding: 0;
-        }
-        .item {
-        width: 100%;
-        height: 30px;
-        background-color: #f3f3f3;
-        border: 1px solid #666;
-        box-sizing: border-box;
-        }
-        .fade-move,
-        .fade-enter-active,
-        .fade-leave-active {
-        transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
-        }
-        .fade-enter-from,
-        .fade-leave-to {
-        opacity: 0;
-        transform: scaleY(0.01) translate(30px, 0);
-        }
-        .fade-leave-active {
-        position: abs
-        `
    })
    
    app.mount('#app')
