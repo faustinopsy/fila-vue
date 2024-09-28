023793b7c18d7679141f5c18127c4218323a7db7
faustinopsy <rodrigohipnose@gmail.com>
2024-09-28 13:54:44 -0300

    iniciando lista de compras

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 const app = Vue.createApp({
     data() {
-     return {
-      message: "Hello World!"
-     }
-    }
+        return {
+            nome: null,
+            quantidade: null,
+            carrinho: [
+              { id: 1, produto: 'café', qtd: 5 }
+            ]
+          }
+        },
+        template: 
+        `
+        <form>
+            <p>Lista de itens</p>
+            <p>Nome do item: <input type="text" required v-model="nome"></p>
+            <p>Quantos: <input type="number" v-model="quantidade"></p>
+            <button type="submit">Adicionar</button>
+        </form>
+        <p>Carrinho</p>
+        <ul>
+            <li v-for="item in carrinho">{{item.produto}}, {{item.qtd}}</li>
+        </ul>
+        `
    })
    
    app.mount('#app')
