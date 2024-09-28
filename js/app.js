b0b88e81fd7d226b83c913542d71903dc331b101
faustinopsy <rodrigohipnose@gmail.com>
2024-09-28 14:23:48 -0300

    adicionando o metodo remover item

--- js/app.js
+++ js/app.js
@@ -3,24 +3,30 @@ const app = Vue.createApp({
         return {
             nome: null,
             quantidade: null,
-            carrinho: [
-              { id: 1, produto: 'café', qtd: 5 }
-            ]
+            carrinho: []
           }
         },
         methods: {
             adicionarItem() {
               let item = {
+                id: Date.now(),
                 produto: this.nome,
                 qtd: this.quantidade
               }
               this.carrinho.push(item)
               this.nome = null
               this.quantidade = null
+            },
+            removerItem(item) {
+              const i = this.carrinho.indexOf(item)
+              if (i > -1) {
+                this.carrinho.splice(i, 1)
+              }
             }
           },
         template: 
         `
+        <div class="container">
         <form v-on:submit.prevent="adicionarItem">
             <p>Lista de itens</p>
             <p>Nome do item: <input type="text" required v-model="nome"></p>
@@ -29,8 +35,35 @@ const app = Vue.createApp({
         </form>
         <p>Carrinho</p>
         <ul>
-            <li v-for="item in carrinho">{{item.produto}}, {{item.qtd}}</li>
+            <li v-for="item in carrinho" >{{item.id}}, {{item.produto}}, {{item.qtd}} <button @click="removerItem(item)">x</button></li>
         </ul>
+        </div>
+        `,
+        style:
+        `
+        .container {
+        position: relative;
+        padding: 0;
+        }
+        .item {
+        width: 100%;
+        height: 30px;
+        background-color: #f3f3f3;
+        border: 1px solid #666;
+        box-sizing: border-box;
+        }
+        .fade-move,
+        .fade-enter-active,
+        .fade-leave-active {
+        transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+        }
+        .fade-enter-from,
+        .fade-leave-to {
+        opacity: 0;
+        transform: scaleY(0.01) translate(30px, 0);
+        }
+        .fade-leave-active {
+        position: abs
         `
    })
    
