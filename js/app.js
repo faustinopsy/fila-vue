1e701b56d88a2147c8db016f5c17fe2f00a22a61
faustinopsy <rodrigohipnose@gmail.com>
2024-09-28 16:14:34 -0300

    complentando os metodos adicionar e remover

--- js/app.js
+++ js/app.js
@@ -5,17 +5,73 @@ class Item{
         this.carrinho = [];
         this.app = document.querySelector("#app")
         this.template()
+        this.atualizarUI()
     }
-    adicionarItem(){
-
+    adicionarItem() {
+        this.nome = document.querySelector("#nome").value;
+        this.quantidade = document.querySelector("#quantidade").value;
+        let item = {
+          id: Date.now(),
+          produto: this.nome,
+          qtd: this.quantidade
+        }
+        this.carrinho.push(item)
+        this.nome = null
+        this.quantidade = null
+      }
+    removerItem(id) {
+        const index = this.carrinho.findIndex(item => item.id === parseInt(id));
+        console.log(index)
+        if (index !== -1) {
+            this.carrinho.splice(index, 1);
+            this.atualizaLista()
+        } else {
+            console.log(`Item com id ${id} não encontrado.`);
+        }
     }
-    removerItem(){
-
+    atualizarUI(){
+        const form = document.querySelector("#adicionarItem")
+        form.addEventListener("submit", (e)=>{
+            e.preventDefault();
+            this.adicionarItem();
+            this.atualizaLista();
+        });
+    }
+    atualizaLista(){
+        const listaUI = document.querySelector("#lista")
+            listaUI.innerHTML = ""
+            this.carrinho.forEach(item => {
+                const li = document.createElement("li")
+                li.innerHTML = ` ${item.produto} , ${item.qtd} <button id="remove" data-id="${item.id}"> X </button>`
+                listaUI.appendChild(li);
+            });
+            document.querySelector("#nome").value = '';
+            document.querySelector("#quantidade").value = '';
+            this.afterRender()
+    }
+    afterRender() {
+        const removeButtons = document.querySelectorAll('#remove');
+        removeButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                const id = e.target.getAttribute('data-id');
+                this.removerItem(id)
+            });
+        });
     }
     template(){
         this.app.innerHTML = 
         `
-        teste
+        <div class="container">
+        <form id="adicionarItem">
+            <p>Lista de itens</p>
+            <p>Nome do item: <input type="text" required id="nome"></p>
+            <p>Quantos: <input type="number" id="quantidade"></p>
+            <button >Adicionar</button>
+        </form>
+        <p>Carrinho</p>
+            <ul id="lista">
+            </ul>
+        </div>
         `
     }
  }
