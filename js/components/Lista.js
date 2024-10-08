45c92ed25cf7dfc6a4c31d933a07cb2cf58059b8
faustinopsy <rodrigohipnose@gmail.com>
2024-10-08 17:17:52 -0300

    colocando o v-if na listagem

--- js/components/Lista.js
+++ js/components/Lista.js
@@ -1,8 +1,8 @@
 export default {
   template: `
-    <ul>
-      <li v-for="item in carrinho" :key="item.id">
-        {{item.id}}, {{item.produto}}, {{item.qtd}}
+    <ul v-for="item in carrinho" :key="item.id">
+      <li v-if="item.qtd > 10">
+        {{item.id}}, {{item.produto}}, {{item.qtd}} 
         <button @click="$emit('remover-item', item)">x</button>
       </li>
     </ul>
